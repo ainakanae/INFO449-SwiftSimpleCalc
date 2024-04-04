88ce50bfeb7933f6f2fa53803385e28c237a3d5b
Aina Engelbrekt <ainae@uw.edu>
2024-04-03 22:57:38 -0700

    finished!

--- SimpleSwift.playground/Pages/Homework.xcplaygroundpage/Contents.swift
+++ SimpleSwift.playground/Pages/Homework.xcplaygroundpage/Contents.swift
@@ -27,11 +27,100 @@ print("Welcome to the UW Calculator Playground")
 //: For this latter set of operations, it is safe to assume that `["count"]` (with no additional arguments) is 0, `["avg"]` is also 0, and `["fact"]` is 0. `["1", "fact"]` should return 1, and `["0", "fact"]` should also return 1. (Yes, 0-factorial is 1. True story.)
 //: 
 func calculate(_ args: [String]) -> Int {
+    
+    //See if we are doing simple calculations or doing the extra "count", "avg", etc
+    let lastString = args.last
+
+    //if the last thing can be converted into an int, it is the second int of the two numbers you are dealing with,
+    //so store both the integers so we can math them
+    if let notCount = Int(lastString ?? "") {
+        
+        let secondInt = notCount
+        let firstInt = Int(args.first ?? "")
+        
+        //find the operator
+        let plusMinus = args[1]
+        
+        if plusMinus == "+" {
+            return (firstInt ?? 0) + secondInt
+        } else if plusMinus == "-"{
+            return (firstInt ?? 0) - secondInt
+        } else if plusMinus == "*" {
+            return (firstInt ?? 0) * secondInt
+        } else if plusMinus == "/" {
+            if secondInt != 0 {
+                return (firstInt ?? 0) / secondInt
+            } else {
+                return -1
+                }
+        } else if plusMinus == "%" {
+                return (firstInt ?? 0) % secondInt
+        } else {
+                return -1
+        }
+     //the else is for if i was not able to make it into an integer, so i know I am using the other functions
+    } else {
+        //taking the last string and setting it as my command variable
+        let command = lastString ?? ""
+        
+        if command == "count" {
+            return args.count - 1
+        } else if command == "avg" {
+            if args.count <= 1 {
+                //nothing to average
+                return 0
+            }
+            //take out the last one and add them all together
+            let numbers = args.dropLast().compactMap { Int($0) }
+            let total = numbers.reduce(0, +)
+            return total / numbers.count
+        
+        } else if command == "fact" {
+            //special case of if theres no numbers to factorial
+            if args.count == 1 {
+                return 0
+            }
+            // Factorial calculation
+            let num = Int(args.first ?? "") ?? 0
+            if num < 0 {
+                return 0 // just return 0 for the 0
+            } else if num == 0 || num == 1 {
+                return 1 // keeping in mind of specical cases with the 0 case
+            } else {
+                return (1...num).reduce(1, *)
+            }
+        }
+    }
+
     return -1
 }
 
 func calculate(_ arg: String) -> Int {
-    return -1
+    
+    //were taking the string so we can make it an array of strings
+    //initialize the array and the individual words
+    var brokenString: [String] = []
+    var currentString = ""
+    
+    //look for the space character so we know where to break it
+    for char in arg {
+        if char == " " {
+            brokenString.append(currentString)
+            // add that chunk and set it back to blank so we can do it again
+            currentString = ""
+        } else {
+           //add it to the string
+            currentString.append(char)
+        }
+    }
+    
+    // add it all to the array!
+    if !currentString.isEmpty {
+        brokenString.append(currentString)
+    }
+    
+    //just call the first one we made.
+    return calculate(brokenString)
 }
 
 //: Below this are the test expressions/calls to verify if your code is correct.
